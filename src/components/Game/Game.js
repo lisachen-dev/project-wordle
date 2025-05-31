f69aacd890f1467e1c079bfc29f4b39f59c7aeaf
Lisa <lisa.hl.chen@gmail.com>
2025-05-31 12:48:37 -0700

    refactor: reset

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,36 +3,13 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
-import GuessInput from '../GuessInput';
-import GuessResults from '../GuessResults';
-
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-    const [ enteredVal, setEnteredVal ] = React.useState('');
-    const [ resultList, setResultList ] = React.useState([]);
-
-    function handleAdd(word){
-        const wordToAdd = {
-            id: crypto.randomUUID(),
-            word
-        }
-        setResultList([...resultList, wordToAdd])
-    }
-
-    return(
-        <>
-            <GuessInput
-                enteredVal={enteredVal}
-                setEnteredVal={setEnteredVal}
-                handleAdd={handleAdd}
-            />
-            <GuessResults resultList={resultList} />
-        </>
-    )
+  return <>Put a game here!</>;
 }
 
 export default Game;
--- src/components/GuessRow/index.js
+++
@@ -1,2 +0,0 @@
-export * from './GuessRow';
-export { default } from './GuessRow';
